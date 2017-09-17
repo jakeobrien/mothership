857b4e7781920d48f4d1ce9932a64d68c904f4f2
Jake OBrien <neuronrain@gmail.com>
2017-09-17 03:28:00 -0400

    more riggin’

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Magnet : MonoBehaviour {
+	public bool isActive;
 	public LayerMask _environmentLayers;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+
+	private Transform _target;
+
 	void Update () {
-		
+		if (_target == null) return;
+		if (isActive)
+		{
+			_target = null;
+			return;
+		}
+        _target.position = this.transform.position;
 	}
 
 	private void OnCollisionEnter2D(Collision2D coll)
     {
+		if (!isActive) return;
         if (!_environmentLayers.ContainsLayer(coll.gameObject.layer)) return;
-        coll.gameObject.transform.position = this.transform.position;
+		_target = coll.transform;
     }
 }
