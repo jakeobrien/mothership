439cdedbff8af6ae089cef36763ed54ea956c601
Jake OBrien <neuronrain@gmail.com>
2017-09-17 08:55:33 -0400

    audiopool

---
+++ Assets/Scripts/AudioPool.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+
+public class AudioPool : MonoBehaviour
+{
+	public AudioMixerGroup mixerGroup;
+    private List<AudioSource> _sources;
+    private int _maxSources = 6;
+
+    public void Start()
+    {
+    	_sources = new List<AudioSource>();
+    }
+
+    public AudioSource GetAudioSourceFromPool()
+    {
+        foreach (AudioSource source in _sources)
+		{
+            if (!source.isPlaying) return source;
+        }
+		if (_sources.Count >= _maxSources)
+		{
+			Debug.LogWarning("NO HIT AUDIOSOURCES AVAILABLE. ADD MORE SOURCES TO POOL");
+			return null;
+		}
+        var newSource = gameObject.AddComponent<AudioSource>();
+		if (mixerGroup != null) newSource.outputAudioMixerGroup = mixerGroup;
+        _sources.Add(newSource);
+        return newSource;
+    }
+
+}
