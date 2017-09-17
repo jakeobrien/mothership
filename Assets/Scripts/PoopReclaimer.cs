5c146b675a398cfaf163bb777e20ca3e596b4e2a
Jake OBrien <neuronrain@gmail.com>
2017-09-17 12:28:56 -0400

    poop reclaiming

---
+++ Assets/Scripts/PoopReclaimer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoopReclaimer : MonoBehaviour
+{
+
+
+	private void OnTriggerStay2D(Collider2D coll)
+	{
+		var poop = coll.GetComponent<Poop>();
+		if (poop != null) Destroy(poop.gameObject);
+	}
+
+}
