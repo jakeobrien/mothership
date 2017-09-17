ebc26e1f9885875e98f21a56beed47092262978c
spacebit <swpett10@gmail.com>
2017-09-17 02:39:52 -0400

    simple magnet

---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour {
+	public LayerMask _environmentLayers;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (!_environmentLayers.ContainsLayer(coll.gameObject.layer)) return;
+        coll.gameObject.transform.position = this.transform.position;
+    }
+}
