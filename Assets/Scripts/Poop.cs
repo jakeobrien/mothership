397e31afc1a59a12096c686c5ed133b885ee7caa
Jake OBrien <neuronrain@gmail.com>
2017-09-17 11:25:26 -0400

    some basic poop stuff

---
+++ Assets/Scripts/Poop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Poop : MonoBehaviour
+{
+
+	public static Action PoopExploded;
+
+	public float _durationToExplode;
+	public GameObject _explosionPrefab;
+	private float _startTime;
+
+	private void Update()
+	{
+		if (Time.time - _startTime > _durationToExplode)
+		{
+			Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+			Destroy(gameObject);
+			if (PoopExploded != null) PoopExploded();
+		}
+	}
+
+}
